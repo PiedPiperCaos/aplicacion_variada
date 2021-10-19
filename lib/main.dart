0a9662f0dab810a45ee9554138c5475ba978a63c
PiedPiperCaos <cmarmeg382@g.educaand.es>
2021-10-19 14:30:42 +0200

    Final clase martes 19/10

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:aplicacion_variada/src/pages/alert_page.dart';
+import 'package:aplicacion_variada/src/pages/animated_container.dart';
 import 'package:aplicacion_variada/src/pages/card_page.dart';
 import 'package:aplicacion_variada/src/pages/avatar_page.dart';
 import 'package:aplicacion_variada/src/pages/home_page.dart';
+import 'package:aplicacion_variada/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -19,6 +21,8 @@ class MyApp extends StatelessWidget {
         'alert': (BuildContext context) => AlertPage(),
         'avatar': (BuildContext context) => AvatarPage(),
         'card': (BuildContext context) => CardPage(),
+        'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+        'inputs': (BuildContext context) => InputPage(),
       },
     );
   }
---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _nombre = '';
+  String _email = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs de texto'),
+      ),
+      body: ListView(
+        children: [
+          _crearInput(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPersona(),
+        ],
+      ),
+    );
+  }
+
+  _crearInput() {
+    return TextField(
+      // autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Nombre de la persona',
+        labelText: 'Nombre',
+        helperText: 'Solo es el nombre',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
+      ),
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+        });
+      },
+    );
+  }
+
+  _crearPersona() {
+    return ListTile(
+      title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email es: $_email'),
+    );
+  }
+
+  _crearEmail() {
+    return TextField(
+      // autofocus: true,
+      keyboardType: TextInputType.emailAddress,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Direccion de email',
+        labelText: 'E-mail',
+        helperText: 'Su direccion de correo',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
+      ),
+      onChanged: (valor) {
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+}
